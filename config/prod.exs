a667723f760fef5b7252307fcb6fe1762e37df9b
Max Neuvians <max@neuvians.net>
2019-03-17 20:31:33 -0400

    Force rebuild

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,6 @@ use Mix.Config
 config :loon, LoonWeb.Endpoint,
   url: [scheme: "https", host: "loon-server.herokuapp.com", port: System.get_env("PORT")],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
