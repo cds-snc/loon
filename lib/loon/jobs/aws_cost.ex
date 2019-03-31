6bd495986213d136bc60ea2a6444df058829839a
Max Neuvians <max@neuvians.net>
2019-03-31 12:02:05 -0400

    Unify cost call

--- lib/loon/jobs/aws_cost.ex
+++ lib/loon/jobs/aws_cost.ex
@@ -11,66 +11,55 @@ defmodule Loon.Jobs.AwsCost do
   def job do
     client = client()
 
-    {:ok, last_months, _} =
+    {:ok, cost_per_month, _} =
       Loon.Support.CostExplorer.get_cost_and_usage(
         client,
         %{
           Granularity: "MONTHLY",
-          TimePeriod: %{Start: beginning_of_month(-6), End: end_of_month(-1)},
+          TimePeriod: %{Start: beginning_of_month(-6), End: end_of_month()},
           Metrics: ["UnblendedCost"]
         }
       )
 
-      {:ok, current_month, _} =
-        Loon.Support.CostExplorer.get_cost_and_usage(
-          client,
-          %{
-            Granularity: "MONTHLY",
-            TimePeriod: %{Start: beginning_of_month(), End: end_of_month()},
-            Metrics: ["UnblendedCost"]
-          }
-        )
+    {:ok, forecast, _} =
+      Loon.Support.CostExplorer.get_cost_forecaset(
+        client,
+        %{
+          Granularity: "MONTHLY",
+          TimePeriod: %{Start: tomorrow(), End: tomorrows_end_of_month()},
+          Metric: "UNBLENDED_COST"
+        }
+      )
 
-        {:ok, forecast, _} =
-          Loon.Support.CostExplorer.get_cost_forecaset(
-            client,
-            %{
-              Granularity: "MONTHLY",
-              TimePeriod: %{Start: tomorrow(), End: tomorrows_end_of_month()},
-              Metric: "UNBLENDED_COST"
-            }
-          )
-
-    %{last_months: last_months, current_month: current_month, forecast: forecast}
+    %{cost_per_month: cost_per_month, forecast: forecast}
   end
 
   defp beginning_of_month(shift \\ 0) do
-    Timex.now
+    Timex.now()
     |> Timex.shift(months: shift)
-    |> Timex.beginning_of_month
+    |> Timex.beginning_of_month()
     |> Timex.format!("%Y-%m-%d", :strftime)
   end
 
   defp end_of_month(shift \\ 0) do
-    Timex.now
+    Timex.now()
     |> Timex.shift(months: shift)
-    |> Timex.end_of_month
+    |> Timex.end_of_month()
     |> Timex.format!("%Y-%m-%d", :strftime)
   end
 
   defp tomorrow() do
-    Timex.now
+    Timex.now()
     |> Timex.shift(days: 1)
     |> Timex.format!("%Y-%m-%d", :strftime)
   end
 
   defp tomorrows_end_of_month() do
-    Timex.now
+    Timex.now()
     |> Timex.shift(days: 1)
-    |> Timex.end_of_month
+    |> Timex.end_of_month()
     |> Timex.format!("%Y-%m-%d", :strftime)
   end
-
 
   defp client do
     %AWS.Client{
