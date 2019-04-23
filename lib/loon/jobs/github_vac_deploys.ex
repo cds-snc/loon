ed7b86ea36a8217c09c07cc876e331fd3fcb2e6e
smcmurtry <smcmurtry@gmail.com>
2019-04-23 15:07:10 -0400

    added new job

---
+++ lib/loon/jobs/github_vac_deploys.ex
@@ -0,0 +1,24 @@
+defmodule Loon.Jobs.GithubVacDeploys do
+  @moduledoc false
+
+  use Loon.Jobs,
+    description: "Returns GitHub data on merges to master for https://github.com/veteransaffairscanada/vac-benefits-directory every hour",
+    schedule: "0 * * * *"
+
+  @doc """
+  Returns GitHub data on merges to master for https://github.com/veteransaffairscanada/vac-benefits-directory every hour
+  """
+  def job() do
+    token = Map.fetch!(System.get_env(), "GITHUB_PUBLIC_ACCESS_TOKEN")
+    url = "https://api.github.com/graphql"
+    headers = ["Authorization": "Bearer #{token}", "Accept": "Application/json"]
+    query = '{"query": "query {repository(owner: \"veteransaffairscanada\", name: \"vac-benefits-directory\") { pullRequests(baseRefName: \"master\", states: [CLOSED, MERGED], first: 20, after: \"Y3Vyc29yOnYyOpHOCrPLKw==\") {nodes {title mergedAt url} pageInfo { endCursor hasNextPage}}}}"}'
+
+    case HTTPoison.post(url, query, headers) do
+      {:ok, %HTTPoison.Response{body: body}} ->
+        Jason.decode!(body)
+      _ ->
+        false
+    end
+  end
+end
