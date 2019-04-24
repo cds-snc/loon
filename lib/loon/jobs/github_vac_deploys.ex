114829cff4c77c48270fbc87af66561a432c6625
Max <max@neuvians.net>
2019-04-24 10:42:18 -0400

    Changes

--- lib/loon/jobs/github_vac_deploys.ex
+++ lib/loon/jobs/github_vac_deploys.ex
@@ -1,6 +1,6 @@
 defmodule Loon.Jobs.GithubVacDeploys do
   @moduledoc false
-
+  require Logger
   use Loon.Jobs,
     description: "Returns GitHub data on merges to master for https://github.com/veteransaffairscanada/vac-benefits-directory every hour",
     schedule: "0 * * * *"
@@ -9,14 +9,28 @@ defmodule Loon.Jobs.GithubVacDeploys do
   Returns GitHub data on merges to master for https://github.com/veteransaffairscanada/vac-benefits-directory every hour
   """
   def job() do
+    query([])
+  end
+
+  def query(results, endcursor \\ "") do
+
     token = Map.fetch!(System.get_env(), "GITHUB_PUBLIC_ACCESS_TOKEN")
     url = "https://api.github.com/graphql"
     headers = ["Authorization": "Bearer #{token}", "Accept": "Application/json"]
-    body = "{\"query\": \"query {repository(owner: \\\"veteransaffairscanada\\\", name: \\\"vac-benefits-directory\\\") { pullRequests(baseRefName: \\\"master\\\", states: [CLOSED, MERGED], first: 20, after: \\\"Y3Vyc29yOnYyOpHOCrPLKw==\\\") {nodes {title mergedAt url} pageInfo { endCursor hasNextPage}}}}\"}"
+    body = "{\"query\": \"query {repository(owner: \\\"veteransaffairscanada\\\", name: \\\"vac-benefits-directory\\\") { pullRequests(baseRefName: \\\"master\\\", states: [CLOSED, MERGED], first: 100#{endcursor}) {nodes {title mergedAt url} pageInfo { endCursor hasNextPage}}}}\"}"
 
     case HTTPoison.post(url, body, headers) do
       {:ok, %HTTPoison.Response{body: body}} ->
-        Jason.decode!(body)
+        resp = Jason.decode!(body)
+
+        case resp["data"]["repository"]["pullRequests"] do
+          %{"nodes" => nodes, "pageInfo" => %{"endCursor" => cursor, "hasNextPage" => true}} ->
+            query(results ++ nodes, ", after: \\\"#{cursor}\\\"")
+          %{"nodes" => nodes, "pageInfo" => %{"hasNextPage" => false}} ->
+            results ++ nodes
+          _ ->
+            results
+        end
       _ ->
         false
     end
